30acd35e3bceff3d275a02a02990f71bc432ffd5
Zettacamp-Labib-Fakhrijal <114086478+Zettacamp-Labib-Fakhrijal@users.noreply.github.com>
2022-10-12 13:01:23 +0700

    Javascript Day 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var stock = 50;
 function bp(jml, disc, tax) {
     const b = disc > 0 & disc < 100;
     const c = tax > 0 & tax < 100;
@@ -6,12 +7,18 @@ function bp(jml, disc, tax) {
         var price = 50000;
         pdisc = jml * (price - (price * disc / 100));
         ptax = (pdisc + (pdisc * tax / 100));
-        console.log('Judul Buku: ') + book;
+        stock = stock - jml;
+        console.log('Sisa stock Buku: ' + stock);
+        console.log('Judul Buku: ' + book);
         console.log('Harga setelah Diskon = ' + pdisc);
         console.log('Harga setelah Pajak = ' + ptax);
     } else {
         console.log('Harga ada yang salah!!!');
     }
 }
-
-console.log(bp(10, 99, 10));
+var no = 1;
+while (stock > 0) {
+    console.log(no);
+    console.log(bp(10, 50, 10));
+    no++;
+}
