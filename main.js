988a3fb2fb08e6d50fe427bfd7785bd882ffbeaa
Zettacamp-Labib-Fakhrijal <114086478+Zettacamp-Labib-Fakhrijal@users.noreply.github.com>
2022-10-17 11:52:21 +0700

    Javascript Day 5

--- main.js
+++ main.js
@@ -6,56 +6,67 @@ const port = 3000;
   
 app.listen(port);
 
-var stock = 50;
-function bp(jml, disc, tax, crd) {
-    const b = disc > 0 & disc < 100;
-    const c = tax > 0 & tax < 100;
-    const d = crd >= 0 & crd < 7;
-    if (b == true & c == true & d == true) {
-        var book = 'Habis Gelap Terbitlah Terang';
-        var price = 50000;
-        pdisc = jml * (price - (price * disc / 100));
-        ptax = pdisc + (pdisc * tax / 100);
-        stock = stock - jml;
-        console.log('Jumlah Pembelian: ' + jml);
-        console.log('Diskon: ' + disc + ' %');
-        console.log('Pajak: ' + tax + ' %');
-        console.log('Sisa stock Buku: ' + stock);
-        console.log('Judul Buku: ' + book);
-        console.log('Harga setelah Diskon = ' + pdisc);
-        console.log('Harga Total = ' + ptax);
-        if (crd == 0) {
-            console.log('-------------------------------------');
-        } else {
-            console.log('Kredit: ' + crd + ' bulan');
-            var term = [];
-            for (let i = 0; i < crd; i++) {
-                term[i] = ptax / crd;
-            };
-            let due = {
-                bulan: ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November']
-            }
-            const tenggat = due.bulan.slice(0, crd);
-            console.log('Tenggat Waktu: ' + tenggat);
-            const termcrdt = [];
-            term.forEach((termn, idx, term) => {
-                const termobj = {Tagihan: idx+1, credit: termn + (termn * (tax / 100))}
-                termcrdt.push(termobj);
-            });
-            console.log(termcrdt);
-        }
-    } else {
-        stock = 0;
-        console.log('Coba cek inputan nya lagi!!!');
-    }
-}
+// var stock = 50;
+// function bp(jml, disc, tax, crd) {
+//     const b = disc > 0 & disc < 100;
+//     const c = tax > 0 & tax < 100;
+//     const d = crd >= 0 & crd < 7;
+//     if (b == true & c == true & d == true) {
+//         var book = 'Habis Gelap Terbitlah Terang';
+//         var price = 50000;
+//         pdisc = jml * (price - (price * disc / 100));
+//         ptax = pdisc + (pdisc * tax / 100);
+//         stock = stock - jml;
+//         console.log('Jumlah Pembelian: ' + jml);
+//         console.log('Diskon: ' + disc + ' %');
+//         console.log('Pajak: ' + tax + ' %');
+//         console.log('Sisa stock Buku: ' + stock);
+//         console.log('Judul Buku: ' + book);
+//         console.log('Harga setelah Diskon = ' + pdisc);
+//         console.log('Harga Total = ' + ptax);
+//         if (crd == 0) {
+//             console.log('-------------------------------------');
+//         } else {
+//             console.log('Kredit: ' + crd + ' bulan');
+//             var term = [];
+//             for (let i = 0; i < crd; i++) {
+//                 term[i] = ptax / crd;
+//             };
+//             let due = {
+//                 bulan: ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November']
+//             }
+//             const tenggat = due.bulan.slice(0, crd);
+//             console.log('Tenggat Waktu: ' + tenggat);
+//             const termcrdt = [];
+//             term.forEach((termn, idx, term) => {
+//                 const termobj = {Tagihan: idx+1, credit: termn + (termn * (tax / 100))}
+//                 termcrdt.push(termobj);
+//             });
+//             console.log(termcrdt);
+//         }
+//     } else {
+//         stock = 0;
+//         console.log('Coba cek inputan nya lagi!!!');
+//     }
+// }
 
-// app.use((req, res, next) => {
-//     res.send('<h1>ini judul</h1>');
-// });
-// app.use((req, res, next) => {
-//     res.send('<h1>Get World!</h1><form method="get" action="/"><input name="Jumlah" type="text"><button type="submit">Itung</button></form>');
-// });
+const checkAuth = (req, res, next) => {
+    const auth = req.headers["authorization"];
+
+    const userpass = auth.split(" ")[1];
+
+    const text = Buffer.from(userpass, "base64").toString("ascii");
+
+    const username = text.split(":")[0];
+    const password = text.split(":")[1];
+  
+    if (username == process.env.USERNAME && password == process.env.PASSWORD) 
+    {
+      return next();
+    } else {
+      return res.json("Access Denied.");
+    }
+  };
 
 var parbook = {
     jumlah: 10,
@@ -64,8 +75,6 @@ var parbook = {
     kredit: 3
 }
 app.get('/book', (req, res) => {
-    // var jml = req.params.jml;
-    // var dsc = 
     res.send(parbook);
 });
 app.post('/', (req, res) => {
